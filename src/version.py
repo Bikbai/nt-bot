1549ccf297bb5b90e1bddc996c2717fa50ffbf83
Demidov <demidov.roman@gmail.com>
2022-10-06 14:42:47 +0300

    refactoring

--- src/version.py
+++ src/version.py
@@ -3,6 +3,6 @@ import utility as u
 
 
 def show_version():
-    repo = git.Repo(search_parent_directories=True)
+    repo = git.Repo(search_parent_directories=True, path='./nt-bot/')
     sha = repo.head.object.hexsha
     u.log_info(f"Running version {sha}")
