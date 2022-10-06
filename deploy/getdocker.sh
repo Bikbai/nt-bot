ec19de34bc72c85f64889b1faaa95e7a310241b5
Demidov <demidov.roman@gmail.com>
2022-10-06 14:51:57 +0300

    refactoring

--- deploy/getdocker.sh
+++ deploy/getdocker.sh
@@ -1,2 +1,5 @@
- curl -fsSL https://get.docker.com -o get-docker.sh
- DRY_RUN=1 sh ./get-docker.sh
+curl -fsSL https://get.docker.com -o get-docker.sh
+DRY_RUN=1 sh ./get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)"  -o /usr/local/bin/docker-compose
+sudo mv /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo chmod +x /usr/bin/docker-compose
